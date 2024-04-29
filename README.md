ab3c58e08ee4471af7553ad217e667b24e580b6a
sakibZeem <anamulsakibzim22@gmail.com>
2024-04-29 23:59:45 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Group 12
+
+## Students
+
+1. **Name:** Ali Daud Hossain   
+   **ID:** 1930544
+
+2. **Name:** Komol Karmakar  
+   **ID:** 1930048
+
+3. **Name:** Md. Anamul Sakib Zim  
+   **ID:** 1931107
+
+
+## Description
+
+Our final project E-commerce  
---
+++ lib/screens/bottom_nav_pages/favourite.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_commerce/services/fetchProducts.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Favourite extends StatefulWidget {
+  @override
+  _FavouriteState createState() => _FavouriteState();
+}
+
+class _FavouriteState extends State<Favourite> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: fetchData("users-favourite-items"),
+      ),
+    );
+  }
+}
